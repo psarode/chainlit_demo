284d2a1b333649d846c7556c1c5a393df05852a0
Priyadarshini Sarode <priya@Priyadarshinis-MacBook-Pro.local>
2024-09-04 17:55:07 -0700

    setup openAi

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 import chainlit as cl
+import openai
+import os
+api_key = os.getenv("OPENAI_API_KEY")
+
+endpoint_url = "https://api.openai.com/v1"
+client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
+
+# https://platform.openai.com/docs/models/gpt-4o
+model_kwargs = {
+    "model": "chatgpt-4o-latest",
+    "temperature": 0.3,
+    "max_tokens": 500
+}
 
 @cl.on_message
 async def on_message(message: cl.Message):
     # Your custom logic goes here...
+    response = await client.chat.completions.create(
+        messages=[{"role": "user", "content": message.content}],
+        **model_kwargs
+    )
+
+    # https://platform.openai.com/docs/guides/chat-completions/response-format
+    response_content = response.choices[0].message.content
 
     # Send a response back to the user
     await cl.Message(
